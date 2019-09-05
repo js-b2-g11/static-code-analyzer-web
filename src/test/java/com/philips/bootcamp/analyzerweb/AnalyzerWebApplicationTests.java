47e54ae6f06b077b455f7c20844466fa1f20f7f5
Vrushab Shandilya <vrushab.shandilya@philips.com>
2019-09-05 13:39:44 +0530

    add analyzer classes invalid test cases

--- src/test/java/com/philips/bootcamp/analyzerweb/AnalyzerWebApplicationTests.java
+++ src/test/java/com/philips/bootcamp/analyzerweb/AnalyzerWebApplicationTests.java
@@ -12,6 +12,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+
+import com.philips.bootcamp.analyzerweb.service.CheckstyleAnalyzer;
+import com.philips.bootcamp.analyzerweb.service.IntegratedAnalyzer;
+import com.philips.bootcamp.analyzerweb.service.PmdAnalyzer;
 import com.philips.bootcamp.analyzerweb.utils.PathEncoder;
 import com.philips.bootcamp.analyzerweb.utils.Values;
 
@@ -19,48 +23,69 @@ import com.philips.bootcamp.analyzerweb.utils.Values;
 @SpringBootTest
 public class AnalyzerWebApplicationTests {
 
+	@Test
+	public void getReportOfValidFileDirectoryForPmd() throws Exception {
+		final String testUri = "/api/pmd/?path=" + PathEncoder.encodeURI(Values.TEST_VALID_FILE_PATH);
+		final RestTemplate restTemplate = new RestTemplate();
+		final String baseUrl = "http://localhost:8080" + testUri;
+		final URI uri = new URI(baseUrl);
+		final ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+		assertEquals(200, result.getStatusCodeValue());
+	}
 
-  @Test
-  public void getReportOfValidFileDirectoryForPmd() throws Exception{
-    final String testUri = "/api/pmd/?path="+PathEncoder.encodeURI(Values.TEST_VALID_FILE_PATH);
-    final RestTemplate restTemplate = new RestTemplate();
-    final String baseUrl = "http://localhost:8080" + testUri;
-    final URI uri = new URI(baseUrl);
-    final ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
-    assertEquals(200, result.getStatusCodeValue());
-  }
+	@Test
+	public void getReportOfValidFileDirectoryForCheckstyle() throws Exception {
+		final String testUri = "/api/cs/?path=" + PathEncoder.encodeURI(Values.TEST_VALID_FILE_PATH);
+		final RestTemplate restTemplate = new RestTemplate();
+		final String baseUrl = "http://localhost:8080" + testUri;
+		final URI uri = new URI(baseUrl);
+		final ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+		assertEquals(200, result.getStatusCodeValue());
+	}
 
-  @Test
-  public void getReportOfValidFileDirectoryForCheckstyle() throws Exception{
-    final String testUri = "/api/cs/?path="+PathEncoder.encodeURI(Values.TEST_VALID_FILE_PATH);
-    final RestTemplate restTemplate = new RestTemplate();
-    final String baseUrl = "http://localhost:8080" + testUri;
-    final URI uri = new URI(baseUrl);
-    final ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
-    assertEquals(200, result.getStatusCodeValue());
-  }
+	@Test(expected = HttpClientErrorException.class)
+	public void getReportOfInvalidFileDirectoryForPmd() throws Exception {
+		final String testUri = "/api/pmd/?path=" + PathEncoder.encodeURI(Values.TEST_INVALID_FILE_PATH);
+		final RestTemplate restTemplate = new RestTemplate();
+		final String baseUrl = "http://localhost:8080" + testUri;
+		final URI uri = new URI(baseUrl);
+		ResponseEntity<String> result = null;
+		result = restTemplate.getForEntity(uri, String.class);
+		assertEquals(404, result.getStatusCodeValue());
+	}
 
-  @Test(expected = HttpClientErrorException.class)
-  public void getReportOfInvalidFileDirectoryForPmd() throws Exception{
-    final String testUri = "/api/pmd/?path="+PathEncoder.encodeURI(Values.TEST_INVALID_FILE_PATH);
-    final RestTemplate restTemplate = new RestTemplate();
-    final String baseUrl = "http://localhost:8080" + testUri;
-    final URI uri = new URI(baseUrl);
-    ResponseEntity<String> result = null;
-    result = restTemplate.getForEntity(uri, String.class);
-    assertEquals(404, result.getStatusCodeValue());
-  }
+	@Test(expected = HttpClientErrorException.class)
+	public void getReportOfInvalidFileDirectoryForCheckstyle() throws Exception {
+		final String testUri = "/api/cs/?path=" + PathEncoder.encodeURI(Values.TEST_INVALID_FILE_PATH);
+		final RestTemplate restTemplate = new RestTemplate();
+		final String baseUrl = "http://localhost:8080" + testUri;
+		final URI uri = new URI(baseUrl);
+		ResponseEntity<String> result = null;
+		result = restTemplate.getForEntity(uri, String.class);
+		assertEquals(404, result.getStatusCodeValue());
+	}
 
-
-  @Test(expected = HttpClientErrorException.class)
-  public void getReportOfInvalidFileDirectoryForCheckstyle() throws Exception{
-    final String testUri = "/api/cs/?path="+PathEncoder.encodeURI(Values.TEST_INVALID_FILE_PATH);
-    final RestTemplate restTemplate = new RestTemplate();
-    final String baseUrl = "http://localhost:8080" + testUri;
-    final URI uri = new URI(baseUrl);
-    ResponseEntity<String> result = null;
-    result = restTemplate.getForEntity(uri, String.class);
-    assertEquals(404, result.getStatusCodeValue());
-  }
-
+	@Test(expected = RuntimeException.class)
+	public void generateReport_CheckstyleAnalyzer_InvalidFilePath_ExceptionThrown() {
+		CheckstyleAnalyzer checkstyleTool = new CheckstyleAnalyzer(Values.TEST_INVALID_FILE_PATH, Values.CHECKSTYLE_PATH,
+				Values.CHECKSTYLE_RULESET);
+		checkstyleTool.generateReport();
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void generateReport_PmdAnalyzer_InvalidFilePath_ExceptionThrown() {
+		PmdAnalyzer pmdTool = new PmdAnalyzer(Values.TEST_INVALID_FILE_PATH, Values.PMD_RULESET);
+		pmdTool.generateReport();
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void generateReport_IntegratedAnalyzer_InvalidFilePath_ExceptionThrown() {
+		PmdAnalyzer pmdTool = new PmdAnalyzer(Values.TEST_INVALID_FILE_PATH, Values.PMD_RULESET);
+		CheckstyleAnalyzer checkstyleTool = new CheckstyleAnalyzer(Values.TEST_INVALID_FILE_PATH, Values.CHECKSTYLE_PATH,
+				Values.CHECKSTYLE_RULESET);
+		IntegratedAnalyzer integratedAnalyzer = new IntegratedAnalyzer(Values.TEST_INVALID_FILE_PATH);
+		integratedAnalyzer.add(checkstyleTool);
+		integratedAnalyzer.add(pmdTool);
+		integratedAnalyzer.generateReport();
+	}
 }
