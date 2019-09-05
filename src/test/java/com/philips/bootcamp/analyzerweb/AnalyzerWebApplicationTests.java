84a3fd61bbafe02ec4da911a8fa24fe0dcbde77a
Chiranjeev <jeev.chiran@gmail.com>
2019-09-05 11:05:10 +0530

    add testcases

--- src/test/java/com/philips/bootcamp/analyzerweb/AnalyzerWebApplicationTests.java
+++ src/test/java/com/philips/bootcamp/analyzerweb/AnalyzerWebApplicationTests.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import com.philips.bootcamp.analyzerweb.utils.PathEncoder;
 import com.philips.bootcamp.analyzerweb.utils.ValuesUtil;
@@ -39,23 +40,26 @@ public class AnalyzerWebApplicationTests {
     assertEquals(200, result.getStatusCodeValue());
   }
 
-  @Test
+  @Test(expected = HttpClientErrorException.class)
   public void getReportOfInvalidFileDirectoryForPmd() throws Exception{
     final String testUri = "/api/pmd/?path="+PathEncoder.encodeURI(ValuesUtil.TEST_INVALID_PATH);
     final RestTemplate restTemplate = new RestTemplate();
     final String baseUrl = "http://localhost:8080" + testUri;
     final URI uri = new URI(baseUrl);
-    final ResponseEntity<String> result = null;
+    ResponseEntity<String> result = null;
+    result = restTemplate.getForEntity(uri, String.class);
     assertEquals(404, result.getStatusCodeValue());
   }
 
-  @Test
+
+  @Test(expected = HttpClientErrorException.class)
   public void getReportOfInvalidFileDirectoryForCheckstyle() throws Exception{
     final String testUri = "/api/cs/?path="+PathEncoder.encodeURI(ValuesUtil.TEST_INVALID_PATH);
     final RestTemplate restTemplate = new RestTemplate();
     final String baseUrl = "http://localhost:8080" + testUri;
     final URI uri = new URI(baseUrl);
-    final ResponseEntity<String> result = null;
+    ResponseEntity<String> result = null;
+    result = restTemplate.getForEntity(uri, String.class);
     assertEquals(404, result.getStatusCodeValue());
   }
 
